c785451957bfad35422610db3252353591461cf2
paciente23256 <93782123+paciente23256@users.noreply.github.com>
2022-01-17 15:59:43 +0000

    UPDATE

--- exercicios/exerc_11.py
+++ exercicios/exerc_11.py
@@ -28,63 +28,60 @@ para quebrar a Cifra de César;
 R:
 A análise de frequências é uma técnica simples de criptanálise que consiste em identificar
 os caracteres do texto cifrado usando a frequência de uso dos caracteres na língua em que
-se supõe que a mensagem esteja escrita. O algoritmo de César é um cifrador de substituição 
+se supõe que a mensagem esteja escrita. O algoritmo de César é um cifrador de substituição
 simples e portanto vulnerável a essa técnica.
 
 b. Descubra a mensagem original tendo como fonte o seguinte criptograma:
-“fdrijrcxrufhlrekfufkvljrcjrfcrxizdrjuvgfiklxrcgfikvtilqridfjhlrekrjdrvjtyfirirdhlrekfjwz
-cyfjvdmrfivqrirdhlrekrjefzmrjwztrirdgfitrjrigrirhlvwfjjvjefjjffdrimrcvlrgverklufmrcvr
-gverjvrrcdrerfvgvhlverhlvdhlvigrjjrircvdufsfarufikvdhlvgrjjrircvdurufiuvljrfdrifgvizxf
-vfrszjdfuvldrjevcvvhlvvjgvcyflftvl”
+“fdrijrcxrufhlrekfufkvljrcjrfcrxizdrjuvgfiklxrcgfikvtilqridfjhlrekrjdrvjtyfirirdhlrekfjwzcyfjvdmrfivqrirdhlrekrjefzmrjwztrirdgfitrjrigrirhlvwfjjvjefjjffdrimrcvlrgverklufmrcvrgverjvrrcdrerfvgvhlverhlvdhlvigrjjrircvdufsfarufikvdhlvgrjjrircvdurufiuvljrfdrifgvizxfvfrszjdfuvldrjevcvvhlvvjgvcyflftvl”
 
 R:
-Posicao 25 
+Posicao 25
 """
-#import pyfiglet
+#modulo para o alfabeto
 import string
 
 def encrypt(text, shift):
 
-    encrypted_text = list(range(len(text)))
-    alphabet = string.ascii_lowercase # 'abcdefghijklmnopqrstuvwxyz'
-    first_half = alphabet[:shift]
-    second_half = alphabet[shift:]
-    shifted_alphabet = second_half + first_half
+    msg_encriptada = list(range(len(text)))
+    alfabeto = string.ascii_lowercase # 'abcdefghijklmnopqrstuvwxyz'
+    first_half = alfabeto[:shift]
+    second_half = alfabeto[shift:]
+    shifted_alfabeto = second_half + first_half
 
-    for i, letter in enumerate(text.lower()):
+    for i, letra in enumerate(text.lower()):
 
-        if letter in alphabet:
-            original_index = alphabet.index(letter)
-            new_letter = shifted_alphabet[original_index]
-            encrypted_text[i] = new_letter
+        if letra in alfabeto:
+            original_index = alfabeto.index(letra)
+            nova_letra = shifted_alfabeto[original_index]
+            msg_encriptada[i] = nova_letra
         else:
-            encrypted_text[i] = letter
+            msg_encriptada[i] = letra
 
-    return "".join(encrypted_text)
+    return "".join(msg_encriptada)
 
 def decrypt(text, shift):
     """ quando a posicao ou mudanca e conhecida """
     decrypted_text = list(range(len(text)))
-    alphabet = string.ascii_lowercase
-    first_half = alphabet[:shift]
-    second_half = alphabet[shift:]
-    shifted_alphabet = second_half + first_half
+    alfabeto = string.ascii_lowercase
+    first_half = alfabeto[:shift]
+    second_half = alfabeto[shift:]
+    shifted_alfabeto = second_half + first_half
 
-    for i, letter in enumerate(text.lower()):
+    for i, letra in enumerate(text.lower()):
 
-        if letter in alphabet:
-            index = shifted_alphabet.index(letter)
-            original_letter = alphabet[index]
-            decrypted_text[i] = original_letter
+        if letra in alfabeto:
+            index = shifted_alfabeto.index(letra)
+            original_letra = alfabeto[index]
+            decrypted_text[i] = original_letra
         else:
-            decrypted_text[i] = letter
+            decrypted_text[i] = letra
 
     return "".join(decrypted_text)
 
 def brute_force_decrypt(text):
     """ Se a posicao/mudanca não for conhecida  """
     for n in range(26):
-        print(f"Posicao {n}")
+        print(f"Posição {n}")
         print(decrypt(text, n))
         print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
 
@@ -92,12 +89,20 @@ print("")
 print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
 print("| MESI2022 *CCA-PY* Exercicio nº. 11   b      |")
 print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n")
-TEXTO = input("+ INSERIR TEXTO ENC: " )
-print("")
-#e = encrypt("texto fixo", 8)
-e = encrypt(TEXTO , 8)
-# 'texto'
-print(decrypt(e, 8))
-# 'decrypta a bruta'
-brute_force_decrypt(e)
+TEXTO = input("+ INSERIR MSG A DESENCRIPTAR: " )
+key = int(input("+ ESCOLHA UMA CHAVE: " ))
 
+
+#input_b = int(input())
+
+print("")
+
+#e = encrypt("texto fixo", 8)
+
+# Encripta
+e = encrypt(TEXTO , key)
+# Decripta
+print(decrypt(e, key))
+
+# forca o e
+brute_force_decrypt(e)
