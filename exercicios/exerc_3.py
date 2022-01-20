bd4f7d18a393523ac8f0cfa8b9ff4bed4c4807ba
paciente23256 <93782123+paciente23256@users.noreply.github.com>
2022-01-20 14:23:20 +0000

    Update exerc_3.py

--- exercicios/exerc_3.py
+++ exercicios/exerc_3.py
@@ -4,35 +4,69 @@
 # Alunos #Rui #Pedro #Oscar
 # Exercicio # 3 a) b)
 
-def Cesar(texto, num):
-    alfabeto = [chr(i) for i in range(97,123)]
-    converter_texto_list = []
-    converter_texto = ""
-    was_uppercase = False
-    for caracter in texto:
-        if caracter.isupper():
-            caracter = caracter.lower()
-            was_uppercase = True
-        if caracter not  in alfabeto:
-            converter_texto = converter_texto_list.append(caracter)
-            continue
-        carac_convertido_num = (alfabeto.index(caracter) + num)%26
-        carac_convertido = alfabeto[carac_convertido_num]
-        if was_uppercase:
-            converter_texto_list.append(carac_convertido.upper())
-            was_uppercase = False
-        else:
-            converter_texto_list.append(carac_convertido)
-    converter_texto = "".join(converter_texto_list)
-    return converter_texto
+"""
 
-print("")
-print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
-print("| MESI2022 *CCA-PY* Exercicio nº. 3 a  b      |")
-print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n")
-texto = input("+ Inserir texto [string] a cifrar: ")
-num = int(input("+ Inserir numero para a chave: "))
-texto = Cesar(texto, num)
-print ("+ Texto cifrado:")
-print(texto)
-print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+ Cifra de Caesar
+ É um tipo de cifra de substituição na qual cada letra do texto é substituída
+ por outra, que se apresenta no alfabeto abaixo dela um número fixo de vezes.
+ En(x)=(x+n)mod 26 -> Encriptar
+
+
+"""
+
+from string import ascii_letters
+
+
+"""
+ En(x)=(x+n)mod 26 -> Encriptar
+
+"""
+def encrypt(message, chave):
+    alfabeto = ascii_letters
+    result = ""
+    for char in message:
+        if char not in alfabeto:
+            result += char
+        else:
+            nova_chave = (alfabeto.index(char) + chave) % len(alfabeto)
+            result += alfabeto[nova_chave]
+    return result
+"""
+
+ Dn(x)=(xi-n)mod 26 ->  Desencriptar
+
+"""
+def decrypt(message, chave):
+    return encrypt(message, (chave * -1))
+
+def main():
+    while True:
+        print("")
+        print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+        print("|       MESI2022 *CCA - Criptoanalise*        |")
+        print("|           Exercicio nº. 3 a e b             |")
+        print("|              Cifra de Caesar                |")
+        print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+        print(*["1. Encriptar", "2. Desencriptar", "0. Sair"], sep="\n")
+
+        user_choice = input("Escolha uma opção: ").strip() or "0"
+        print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n")
+
+        if user_choice not in ("1", "2", "0"):
+            print("! ERROR : Escolha uma opção válida!")
+        elif user_choice == "1":
+            message = input("+ Ecriptar Mensagem: ")
+            chave = int(input("+ Inserir Chave: ").strip())
+            print("\nMensagem Encriptada:")
+            print(encrypt(message, chave))
+        elif user_choice == "2":
+            message = input("+ Desencriptar Mensagem: ")
+            chave = int(input("+ Inserir Chave: ").strip())
+            print("\nMensagem Desencriptada:")
+            print(decrypt(message, chave))
+        elif user_choice == "0":
+            print("Obrigado.")
+            break
+
+if __name__ == "__main__":
+    main()
