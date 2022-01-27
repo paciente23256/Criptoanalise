780f7ac44369b7e92f8d51ccd32b6d6a1f6dd0f7
paciente23256 <93782123+paciente23256@users.noreply.github.com>
2022-01-27 19:40:55 +0000

    update

---
+++ exercicios/menu.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# IPBEJA - MESI-2021/2022 - CCA-Criptoanalise - Python
+# Alunos #Rui #Pedro #Oscar
+"""
+MENU
+"""
+
+import runpy
+
+"""atalhos"""
+
+def go_cesar():
+        runpy.run_path('cesar_tool.py')
+        
+def go_playfair():
+        runpy.run_path('playfair_tool.py')
+        
+def go_railfence():
+        runpy.run_path('railfence_tool.py')
+        
+def go_vigenere():
+        runpy.run_path('vigenere_tool.py')
+        
+
+if __name__ == "__main__":
+
+    while True:
+        print("")
+        print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+        print("|       MESI2022 *CCA - Criptoanalise*        |")
+        print("|            Oscar | Pedro | Rui              |")
+        print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+        print(*["\033[37;40m1. Cifra de Cesar Tool", "2. Cifra PlayFair Tool", "3. Cifra RailFence Tool" , "4. Cifra Vigenere Tool" , "0. Sair"], sep="\n")
+        # input do utilizador
+        choice = input("Escolha uma opção: ").strip() or "0"
+
+        print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n")
+
+        # executa as funcoes com base no input do utilizador
+        if choice not in ("1", "2", "3", "4", "0"):
+            print(" ! ERRO. Escolha uma opção válida.")
+        elif choice == "1":
+            go_cesar()
+        elif choice == "2":
+            go_playfair
+        elif choice == "3":
+            go_railfence
+        elif choice == "4":
+            go_vigenere
+        elif choice == "0":
+            print("Adeus.")
+            break
