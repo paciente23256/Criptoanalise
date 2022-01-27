4d0480ae14947bb4b23234ee1404dcb27c2f5960
paciente23256 <93782123+paciente23256@users.noreply.github.com>
2022-01-27 03:17:53 +0000

    uo

---
+++ cifras_ceasar/cesar_decrypt.py
@@ -0,0 +1,37 @@
+'''
+    cesar_decrypt.py
+    given key and caesar ciphertext translate into plaintext
+'''
+import string, sys
+
+def decrypt(key, ciphertext):
+    '''
+        params: char key - offset of A
+                string ciphertext - encoded message to be read
+        desc: decrypt message by reversing offset of each letter by key
+    '''
+    shift = ord(key.upper()) - 65
+    plaintext = ""
+    for c in ciphertext.upper():
+        if ord(c) != ord(" "):
+            nchar = chr(ord(c) - shift)
+            if ord(nchar) < 65:
+                nchar = chr(ord(nchar) + 26)
+            elif ord(nchar) > 90:
+                nchar = chr(ord(nchar) - 26)
+            plaintext += nchar
+        else:
+            plaintext += " "
+    return plaintext
+
+if __name__ == "__main__":
+    try:
+        # automatic mode: get key and ciphertext from commandline options
+        key = sys.argv[1]
+        ciphertext = sys.argv[2]
+    except:
+        # interactive mode: prompt user
+        key = input("Key (char); 'A'= ")
+        ciphertext = input("Ciphertext: ")
+    plaintext = decrypt(key, ciphertext)
+    print(plaintext)
---
+++ cifras_ceasar/cesar_encrypt.py
@@ -0,0 +1,35 @@
+'''
+    cesar_encrypt.py
+    given key, encrypt plaintext into caesar ciphertext
+'''
+import string, sys
+
+def encrypt(key, plaintext):
+    ''' 
+        params: char key - offset of A
+                string plaintext - message to be encoded
+        desc: encrypt message by offsetting each letter by key
+    '''
+    shift = ord(key.upper()) - 65
+    ciphertext = ""
+    for c in plaintext.upper():
+        if ord(c) != ord(" "):
+            nchar = chr(ord(c) + shift)
+            if ord(nchar) > 90:
+                nchar = chr(ord(nchar) - 26)
+            ciphertext += nchar
+        else:
+            ciphertext += " "
+    return ciphertext
+
+if __name__ == "__main__":
+    try:
+        # automatic mode: get key and plaintext from commandline options
+        key = sys.argv[1]
+        plaintext = sys.argv[2]
+    except:
+        # interactive mode: prompt user
+        key = input("Key (char); 'A'= ")
+        plaintext = input("Plaintext: ")
+    ciphertext = encrypt(key, plaintext)
+    print(ciphertext)
