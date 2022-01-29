faf5d569015cc8ba2446895f683bf4db688b63c7
paciente23256 <93782123+paciente23256@users.noreply.github.com>
2022-01-29 01:02:52 +0000

    update

--- exercicios/hack_affine.py
+++ exercicios/hack_affine.py
@@ -1,75 +1,116 @@
+import time, os, sys
+import math
+import re
 
-alphabets = "abcdefghijklmnopqrstuvwxyz"
-alpha_order = dict()
-inv_alpha = dict()
-for i,ch in enumerate(alphabets):
-    alpha_order[ch] = i
-    inv_alpha[i] = ch
-  
-#input for key(a,b) and Plain text
-print("")
-print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
-print("|       MESI2022 *CCA - Criptoanalise*        |")
-print("|            Oscar | Pedro | Rui              |")
-print("|  *Cifra de Affine - Decifra Força Bruta*    |")
-print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
-print("NOTA:- As chaves devem ser primos Relativos  i.e. gcd(a,b) = 1.")   #gcd stands for greatest common divisor.
-key = list(map(int, input("Inserir valor de Alpha e Beta para key(a,b) :").split()))
-plain_text = input("Inserir Texto (letras apenas): ").lower().replace(" ", "")
+status = ''
+S = 'abcdefghijklmnopqrstuvwxyz'
+l = len (S)
 
-encrypted = dict()
-for char in set(plain_text):
-  encrypted[char] = ''
 
-a,b = key[0], key[1]
-for ch in encrypted.keys():
-  val = ( a * alpha_order[ch] + b )  % 26    # Ci = ( a * Pi + b ) mod 26
-  encrypted[ch] = inv_alpha[val]
+# Functions
 
-cipher_text=''
-for ch in plain_text:
-  if ch not in encrypted:
-    cipher_text += ch
-  else:
-    cipher_text += encrypted[ch]
+def validate_status():
+    m = input ('Carregue S para continuar S/n:')
+    if m == 's':
+        outputFile = m
+        outputFile_status (outputFile)
+        st = '0'
+    elif m == '1':
+        outputFile = None
+        st = '1'
+    #return outputfile
+    return st, outputFile
 
-print("Plain text is  :", plain_text)
-print("Cipher text is :", cipher_text)
 
-#To find Greatest Common Divisor using Extended Euclidean Algorithm
-def gcd(a, b): 
-    if a == 0 : 
-        return b  
-    return gcd(b%a, a) 
+def outputFile_status(title):
+    if os.path.lexists (title):
+        #print ('!! The file : %s already exist.You want to continue ? (Y)es or (N)o ? ' % (title))
+        response = input ('> ')
+        if not response.lower ().startswith ('y'):
+            sys.exit ()
 
-# To find a inverse (a^-1) in m(m=26).
-def modInverse(a, m) : 
-    a = a % m; 
-    for x in range(1, m) : 
-        if ((a * x) % m == 1) : 
-            return x 
-    return 1
 
-print("Cipher text is:",cipher_text)
-my_keys = []
-print("Hacked Keys might be:(See at the end,found key)")
-for a in range(1,26,1):
-  for b in range(26):
-    if gcd(a,b)==1:   #as we know only 12*26 = 312 cases are there for Relative Prime.
-      temp = ''
+def writeFile(title, text):
+    oFile = open (title, 'a+')
+    oFile.write ("%s \n" % text)
+    oFile.close ()
 
-      a_inv = modInverse(a,26)
-      for ch in cipher_text:
-        if ch in alphabets:
-          num = (alphabets.find(ch) - b ) *  a_inv     # Pi = [(Ci – b) * a^(-1) ] mod 26
-          num = num % 26
-          if num < 0:
-            num = num + 26
-          temp = temp + alphabets[num]
+
+def makeKeys(k):
+    key1 = k // l
+    key2 = k % l
+    return (key1, key2)
+
+
+def egcd(A, B):
+    if A == 0:
+        return (B, 0, 1)
+    else:
+        g, y, x = egcd (B % A, A)
+        return (g, x - (B // A) * y, y)
+
+
+def modinv(a, m):
+    g, x, y = egcd (a, m)
+    if g != 1:
+        raise Exception ('modular inverse does not exist')
+    else:
+        return x % m
+
+
+def affine_dec(msg, k1, k2, l, status, oF):
+    inv_k1 = modinv (k1, l)
+    msg_decypted = ''
+    for c in msg:
+        if c in S:  # Only characters in 'S'
+            c_index = S.index (c)  # Character's index
+
+            c_dec_index = (c_index - k2) * inv_k1 % l
+
+            msg_decypted += S[c_dec_index]
         else:
-          temp = temp + ch
-      if temp == plain_text:  #when predicted text is same as Plain text, keys has found.
-        my_keys = [a,b]
-      print("key(%d,%d) ::  %s " %(a,b,temp))
+            msg_decypted += c
+    if status == '0':
+        print ("# Alpha  :%s and Beta : %s => %s " % (k1, k2, msg_decypted))
+        writeFile (oF, msg_decypted)
+    return msg_decypted
 
-print("\n Key has found:- keys(%d,%d)"% (my_keys[0],my_keys[1]))
+
+def hack_affine(msg, l, status, oF):
+    for k in range (l ** 2):
+        k1 = makeKeys (k)[0]
+        k2 = makeKeys (k)[1]
+        if math.gcd (k1, l) != 1:
+            continue
+        dec_msg = affine_dec (msg, k1, k2, l, status, oF)
+    return dec_msg
+
+# Main
+
+def Main():
+
+    print("")
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    print("|       MESI2022 *CCA - Criptoanalise*        |")
+    print("|            Oscar | Pedro | Rui              |")
+    print("|         *Cifra de Affine - Tool*            |")
+    print("| Ataque de Força Bruta  C = key1 * P + key2  |")
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    (status, outputFile) = validate_status ()  # outputFile
+    msg = input ('Inserir a mensagem: ')
+    print ('\n')
+    # Infos
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    print ('Lista de Caracteres usados: %s ' % S)
+    print ('\n')
+    print ('A Mensagem inserida : %s ' % msg)
+    print ('\n')
+    print ('A processar ...')
+    timer = time.time ()
+    hack_affine (msg, l, status, outputFile)
+    t = round (time.time () - timer, 2)
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    print ('Tempo de processamento : %s ' % t)
+    print ('\n')
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
